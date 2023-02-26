865c588e294edf1a15de5a1d97238e3db8dfbb00
Chaitali2209 <chaitalipadalkar.it@bvcoenm.edu.in>
2023-02-26 12:52:27 +0530

    first commit

---
+++ client/src/components/Home.js
@@ -0,0 +1,19 @@
+import { useEffect } from "react";
+import HeroSection from "./HeroSection";
+// import { useGlobalContext } from "../context";
+// import Services from "./Services";
+// import Contact from "./Contact";
+
+export default function Home() {
+  
+
+  
+  return (
+    <>
+      <HeroSection />
+      
+    </>
+  );
+};
+
+
